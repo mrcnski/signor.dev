fa68ff396d2b81aed0c9907def6983b6aece7cd0
Marcin S <marcin@bytedude.com>
2021-11-24 20:14:05 -0600

    Clean up code for adventure game engine

--- assets/js/adventure.js
+++ assets/js/adventure.js
@@ -1,19 +1,22 @@
 /*
   This is an adventure game engine I made.
 
+  Have the following libraries loaded:
+  - jquery
+
   Have the following variables defined in a separate script:
   - gameNodes: An object containing all the game nodes.
   - gameNode: A reference to the starting node.
   - inventory: An array containing the starting items.
 
-  Have elements on your page with these IDs:
+  Have div elements on your page with these IDs:
   - #situation-display
   - #choices-display
   - #inventory-display
 
   Have CSS rules for the following classes:
-  - game-choice
-  - game-selectable-item
+  - .game-choice
+  - .game-selectable-item
 
   TODO: outdated
   Game node schema:
@@ -39,17 +42,24 @@
 // List of new items added in the previous choice. Cleared after every turn.
 let newItems = [];
 
+// ==========
+// Game logic
+// ==========
+
+/**
+ * Handle the given choice (i.e. run the game logic).
+ */
 function handleChoice(choice) {
   // Clear all registered events.
   $("*").unbind();
 
-  // Clear all new items.
+  // Clear the list of new items.
   newItems = [];
-
-  // Update inventory.
 
   // Update game node.
   gameNode = gameNodes[choice.next];
+
+  // Update inventory.
   if (gameNode.gainedItems) {
     inventory.push(...gameNode.gainedItems);
     newItems.push(...gameNode.gainedItems);
@@ -59,6 +69,8 @@ function handleChoice(choice) {
       removeItem(item);
     });
   }
+
+  // Run node code.
   if (gameNode.code) {
     gameNode.code();
   }
@@ -67,10 +79,22 @@ function handleChoice(choice) {
   displayGame();
 }
 
+// ============
+// Display code
+// ============
+
+/**
+ * Displays the situation text for the current game node.
+ */
 function displaySituation() {
   let text = gameNode.text;
+
+  // Add the tip if the node has one.
   if (gameNode.tip) {
+    // Get the tip from the tips object.
     const tip = tips[gameNode.tip];
+
+    // Only show the tip if it hasn't been seen before.
     if (!tip.seen) {
       text += `<br><br><i>Tip: ${tip.text}</i>`;
       tip.seen = true;
@@ -80,23 +104,27 @@ function displaySituation() {
   $("#situation-display").html(text);
 }
 
+/**
+ * Displays all choices currently available to the player and registers click
+ * handlers.
+ */
 function displayChoices() {
-  let choices = "";
-
   // Add HTML.
 
-  const classString = `class="game-choice"`;
+  const $choices = $("<ul>");
+
+  // Add all the displayable choices to the #choices-display div.
   let i = 0;
   gameNode.choices.map(choice => {
     if (shouldDisplayChoice(choice)) {
-      const idString = `id="choice${i}"`;
-      choices += `<li ${classString} ${idString}>${choice.text}</li>`;
+      const $choice = $("<li>").addClass("game-choice").attr("id",  `choice${i}`).text(choice.text);
+      $choices.append($choice);
 
       i ++;
     }
   });
 
-  $("#choices-display").html(`<ul>${choices}</ul>`);
+  $("#choices-display").empty().append($choices);
 
   // Add click handlers.
 
@@ -112,20 +140,24 @@ function displayChoices() {
   });
 }
 
-// Display all the items in the inventory.
+/**
+ * Display all the items in the inventory and registers click handlers for any
+ * selectable items.
+ */
 function displayInventory() {
-  let items = "";
   // Keeps track of choices for selectable items.
   const itemChoices = [];
 
+  // Add HTML.
+
+  const $items = $("<ul>");
+
   let i = 0;
   inventory.map(item => {
-    // Add HTML.
-
     let classes = [];
 
-    const new_item = newItems.includes(item);
-    const selectable_item = gameNode.choices.some((choice) => {
+    const is_new_item = newItems.includes(item);
+    const is_selectable_item = gameNode.choices.some((choice) => {
       if (choice.item === item) {
         itemChoices.push([choice, i]);
         return true;
@@ -133,23 +165,21 @@ function displayInventory() {
       return false;
     });
 
-    if (new_item) {
-      item = `<b>${item}</b>`;
+    const $item = $("<li>");
+    if (is_new_item) {
+      $item.css("font-weight","Bold");
     }
-    if (selectable_item) {
-      classes.push("game-selectable-item");
+    if (is_selectable_item) {
+      $item.addClass("game-selectable-item");
     }
 
-    const classString = `class="${classes.join(" ")}"`;
-    const idString = `id="item${i}"`;
-    items += `<li ${classString} ${idString}>${item}</li>`;
+    $item.attr("id", `item${i}`).text(item);
+    $items.append($item);
 
     i ++;
   });
 
-  newItems = [];
-
-  $("#inventory-display").html(`<ul>${items}</ul>`);
+  $("#inventory-display").empty().append($items);
 
   // Add click handlers.
 
@@ -163,14 +193,22 @@ function displayInventory() {
   });
 }
 
+/**
+ * Displays the entire game.
+ */
 function displayGame() {
   displaySituation();
   displayChoices();
   displayInventory();
 }
 
-displayGame();
+// =======
+// Helpers
+// =======
 
+/**
+ * Removes the given item from the inventory.
+ */
 function removeItem(item) {
   const index = inventory.indexOf(item);
   if (index > -1) {
@@ -178,12 +216,23 @@ function removeItem(item) {
   }
 }
 
+/**
+ * Should the choice be displayed?
+ */
 function shouldDisplayChoice(choice) {
+  // If the choice has no text (i.e. it is a selectable item), don't display it.
   if (!choice.text) {
     return false;
   }
+  // Check choice conditional.
   if (choice.if && !choice.if()) {
     return false;
   }
   return true;
 }
+
+// ==========
+// Start game
+// ==========
+
+displayGame();
